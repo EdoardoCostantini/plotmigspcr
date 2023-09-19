51ae5e1d7723786ceb8dbf157bc7a781da62d47a
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-09-19 15:18:09 +0200

    fix: lazy load and data compression

--- data-raw/process-data.R
+++ data-raw/process-data.R
@@ -16,6 +16,6 @@ mids_miexpert <- readRDS("./data-raw/20230829-100551-mids-mi-expert.rds")
 
 # Specify use in app -----------------------------------------------------------
 
-usethis::use_data(estimates, overwrite = TRUE)
-usethis::use_data(mids_migspcr, overwrite = TRUE)
-usethis::use_data(mids_miexpert, overwrite = TRUE)
+usethis::use_data(estimates, overwrite = TRUE, compress = "bzip2")
+usethis::use_data(mids_migspcr, overwrite = TRUE, compress = "bzip2")
+usethis::use_data(mids_miexpert, overwrite = TRUE, compress = "bzip2")
